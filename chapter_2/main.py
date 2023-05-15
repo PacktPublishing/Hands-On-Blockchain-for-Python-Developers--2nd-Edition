802299e8200d5ed71785ae889204429113aac717
Arjuna Sky Kok <sky.kok@lezatify.com>
2023-05-15 22:28:17 +0700

    Add chapter 2 code

---
+++ chapter_2/main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+counter_file = "counter.txt"
+
+if os.path.isfile(counter_file):
+    with open(counter_file, "r") as f:
+        try:
+            counter = int(f.read().strip())
+        except ValueError:
+            counter = 0
+else:
+    counter = 0
+
+@app.get("/")
+async def root():
+    global counter
+    if os.path.isfile(counter_file):
+        with open(counter_file, "r") as f:
+            try:
+                counter = int(f.read().strip())
+            except ValueError:
+                counter = 0
+    counter += 1
+    with open(counter_file, "w") as f:
+        f.write(str(counter))
+    return {"counter": counter}
